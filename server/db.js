9ecb8c9b15c40271677ae6110c0984bc2039e18c
Kat Mota <katmmota@gmail.com>
2025-02-25 15:49:46 -0600

    finish data layer

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const pg = require('pg');
 const uuid = require('uuid');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 
 //set onnection to pg db
-const client = new pg.Client("postgres://kat:Kat1234@localhost:5432/acme_store_db")
+const client = new pg.Client("postgres://kat:Kat1234@localhost:5432/acme_store_db");
 
 const createTables = async () => {
     const SQL = `
@@ -15,46 +15,98 @@ const createTables = async () => {
         CREATE TABLE users(
             id UUID PRIMARY KEY,
             username VARCHAR(50) UNIQUE,
-            password VARCHAR(50)
-        )
+            password VARCHAR(255)
+        );
         CREATE TABLE products(
             id UUID PRIMARY KEY,
-            name VARCHAR(50) UNIQUE,
-        )
+            name VARCHAR(50) UNIQUE
+        );
         CREATE TABLE favorites(
             id UUID PRIMARY KEY,
             product_id UUID REFERENCES products(id) NOT NULL,
             user_id UUID REFERENCES users(id) NOT NULL,
             CONSTRAINT unique_product_user UNIQUE(product_id, user_id)
         )
-    `
+    `;
     await client.query(SQL);
 }
-const createProduct = async () => {
-    
+//A method that creates a product in the database and then returns the created record.
+const createProduct = async (name) => {
+    const SQL = `
+        INSERT INTO products(id, name)
+        VALUES($1,$2)
+        RETURNING *
+    `
+    const result = await client.query(SQL, [uuid.v4(), name]);
+    return result.rows[0];
 }
-const createUser = async () => {
-    
+
+//A method that creates a user in the database and then returns the created record. The password of the user should be hashed by using Bcrypt.
+const createUser = async (username, password) => {
+    const SQL = `
+        INSERT INTO users(id, username, password)
+        VALUES ($1, $2, $3)
+        RETURNING *
+    `
+    const hashedPassword = await bcrypt.hash(password, 5);
+    const result = await client.query(SQL, [uuid.v4(), username,hashedPassword])
+    return result.rows[0];
 }
-const createFavorite = async () => {
-    
+
+//A method that creates a favorite in the database and then returns the created record
+const createFavorite = async (product_id, user_id) => {
+    const SQL = `
+        INSERT INTO favorites(id, product_id, user_id)
+        VALUES($1,$2,$3)
+        RETURNING *
+    `
+    const result = await client.query(SQL, [uuid.v4(), product_id, user_id]);
+    return result.rows[0];
 }
+
+//A method that returns an array of users in the database.
 const fetchUsers = async () => {
-    
+    const SQL = `
+        SELECT * FROM users
+    `;
+    const result = await client.query(SQL);
+    return result.rows;
 }
-const fetchFavorites = async () => {
-    
+
+//A method that returns an array of favorites for a user
+const fetchFavorites = async (user_id) => {
+    const SQL = `
+        SELECT * FROM favorites
+        WHERE user_id = $1
+    `;
+    const result = await client.query(SQL, [user_id]);
+    return result.rows;
 }
+
+//A method that returns an array of products in the database.
 const fetchProducts = async () => {
-    
+    const SQL = `
+        SELECT * FROM products
+    `;
+    const result = await client.query(SQL);
+    return result.rows;
 }
+
+// A method that deletes a favorite in the database.
 const destroyFavorite = async () => {
     
 }
 
-const init = async (params) => {
+const init = async () => {
     await client.connect();
-    createTables()
+    await createTables();
+    const user1 = await createUser("smilingkat", "password");
+    const user2 = await createUser("frowningFrank", "password");
+    const product1 = await createProduct("Summer Friday's Lip Balm");
+    const product2 = await createProduct("Lanolips Donut Glaze Lip Ointment");
+    await createFavorite(product1.id, user2.id);
+    await createFavorite(product2.id, user1.id);
+
 }
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -1,8 +1,36 @@
 
 const db = require('./db');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
+app.get('/api/users', async(req,res,next)=>{
+    try {
+        const result = await db.fetchUsers();
+        res.send(result);
+    } catch (error) {
+        
+    }
+})
+app.get('/api/products', async(req,res,next)=>{
+    try {
+        const result = await db.fetchProducts();
+        res.send(result);
+    } catch (error) {
+        
+    }
+})
+app.get('/api/users/:id/favorites', async(req,res,next)=>{
+    const {id} = req.params
+    try {
+        const result = await db.fetchFavorites(id);
+        res.send(result);
+    } catch (error) {
+        
+    }
+})
 const init = async () => {
-    console.log("App Initialization")
+    app.listen(3000, ()=> console.log('listening on port 3000'))
     db.init();
 }
 init();
